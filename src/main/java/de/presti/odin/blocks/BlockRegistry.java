3db91da2d5b926198bbc0199b53203cdfc3a2bf0
Presti <presti@hoshasei.studio>
2024-03-03 15:25:18 +0100

    Starting with structural stuff.

--- src/main/java/de/presti/odin/blocks/BlockRegistry.java
+++ src/main/java/de/presti/odin/blocks/BlockRegistry.java
@@ -1,6 +1,10 @@
 package de.presti.odin.blocks;
 
 import de.presti.odin.ODIN;
+import de.presti.odin.items.ItemRegistry;
+import de.presti.odin.items.ModCreativeModeTab;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -9,16 +13,27 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class BlockRegistry {
 
     // Create a Deferred Register to hold Blocks which will all be registered under the "odin" namespace
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ODIN.MODID);
 
-    public static final RegistryObject<Block> ODIN_LASER_BLOCK = BLOCKS.register("odin_laser_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> ODIN_LASER_BLOCK = registerBlock("odin_laser_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
 
-    public static void register(IEventBus event) {
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(event);
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(ModCreativeModeTab.ODIN_TAB)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
     }
 
 }
--- src/main/java/de/presti/odin/items/ItemRegistry.java
+++ src/main/java/de/presti/odin/items/ItemRegistry.java
@@ -1,8 +1,6 @@
 package de.presti.odin.items;
 
 import de.presti.odin.ODIN;
-import de.presti.odin.blocks.BlockRegistry;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,11 +10,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemRegistry {
 
     // Create a Deferred Register to hold Blocks which will all be registered under the "odin" namespace
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ODIN.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ODIN.MODID);
 
     public static final RegistryObject<Item> LAUNCH_STICK = ITEMS.register("launch_stick", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ODIN_TAB)));
-    public static final RegistryObject<Item> ODIN_LASER_BLOCK_ITEM = ITEMS.register("odin_laser_block", () -> new BlockItem(BlockRegistry.ODIN_LASER_BLOCK.get(),
-            new Item.Properties().tab(ModCreativeModeTab.ODIN_TAB)));
 
     public static void register(IEventBus event) {
         // Register the Deferred Register to the mod event bus so blocks get registered
